ab790094335204e4819ad82ba6fa6856e697758c
Sonu Kumar Singh <91234141+hiersonusingh@users.noreply.github.com>
2023-06-07 11:44:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,90 +1,129 @@
-// const arr = ['zero', 'one', 'two'];
+/*
+Object Oriented Programming : 
+class : template/Blueprint  - user defined data type
+Object : instance of class - member of class
+inheritance : Parent class -> Child Class
+Encapsulation : Wrapping of data
+Abstraction : Hiding of unnecessary properties from user
+Polymorphism : more than one form
 
-// const obj1 = Object.assign({}, arr);
-// console.log(obj1); // 👉️ {0: 'zero', 1: 'one', 2: 'two'}
+class  : keyword - create a class
 
+constructor : initialize the objects of class
+*/
 
-// const arr = ['zero', 'one', 'two'];
+// class Animal{
+//     display()
+//     {
+//         console.log("This is Animal class")
+//     }
 
-// const obj = {};
-
-// arr.forEach((element, index) => {
-//   obj[`key${index}`] = element;
-// });
-
-// 👇️️ {'key0': 'zero', 'key1': 'one', 'key2': 'two'}
-// console.log(obj);
-
-// const arr = ['zero', 'one', 'two'];
-
-// const obj = {};
-
-// arr.forEach((element, index) => {
-//   obj[index] = element;
-// });
-
-// 👇️️ { '0': 'zero', '1': 'one', '2': 'two' }
-// console.log(obj);
-
-
-// const arr = ['zero', 'one', 'two'];
-
-// const obj = {...arr};
-// console.log(obj); // 👉️ {0: 'zero', 1: 'one', 2: 'two'}
-
-// const arr = [
-//     ['name', 'bobby hadz'],
-//     ['age', 30],
-//   ];
-  
-//   const obj = Object.fromEntries(arr);
-  
-//   console.log(obj); // 👉️ { name: 'bobby hadz', age: 30 }
-  
-// const arr = ['zero', 'one', 'two'];
-
-// const obj = arr.reduce((accumulator, value, index) => {
-//   return {...accumulator, ['key' + index]: value};
-// }, {});
-
-// 👇️️ {'key0': 'zero', 'key1': 'one', 'key2': 'two'}
-// console.log(obj);
-
-
-// const arr = [
-//     {name: 'country', value: 'Austria'},
-//     {name: 'age', value: 30},
-//     {name: 'city', value: 'Vienna'},
-//   ];
-  
-//   const obj = Object.fromEntries(
-//     arr.map(obj => [obj.name, obj.value])
-//   );
-  
-//   👇️ { country: 'Austria', age: 30, city: 'Vienna' }
-//   console.log(obj);
-
-// const arr = [
-//     {name: 'country', value: 'Austria'},
-//     {name: 'age', value: 30},
-//     {name: 'city', value: 'Vienna'},
-//   ];
-  
-  // 👇️ [ [ 'country', 'Austria' ], [ 'age', 30 ], [ 'city', 'Vienna' ] ]
-//   console.log(arr.map(obj => [obj.name, obj.value]));
-
-
-
-// const obj = {};
-
-// const arr = ['bobby', 'hadz', 'com'];
-
-// for (let index = 0; index < arr.length; index++) {
-//   obj[index] = arr[index];
 // }
 
-// 👇️ { '0': 'bobby', '1': 'hadz', '2': 'com' }
-// console.log(obj);
+// const obj = new Animal();
+// obj.display();
 
-  
-  
+// class Programm{
+//     addition(x,y)
+//     {
+//         let sum = x + y;
+//         console.log(sum);
+//     }
+
+// }
+
+
+// const obj = new Programm();
+// obj.addition(25,25);
+
+// class Programm{
+//     addition(x,y)
+//     {
+//         let sum = x + y;
+//         return sum;
+//     }
+
+// }
+
+
+// const obj = new Programm();
+// const res = obj.addition(25,25);
+// console.log(res);
+
+// class Programm{
+//     constructor(x,y)
+//     {
+//         let sum = x + y;
+//         console.log(sum);
+//     }
+// }
+
+// const res = new Programm(28,72);
+
+
+// class Programm{
+//     constructor()
+//     {
+//         console.log("This is default constructor");
+//     }
+// }
+
+// const res = new Programm();
+
+
+// class Shape{
+//     areaOfRectangle(l,b){
+//         return l*b;
+
+//     }
+//     volumeOfCube(l,b,h){
+//         return l*b*h;
+
+//     }
+// }
+
+
+// const  obj = new Shape();
+// const res = obj.areaOfRectangle(25,26);
+// const res1 = obj.volumeOfCube(15,15,15);
+// console.log(res);
+// console.log(res1);
+
+// class Animal{
+//     sleep()
+//     {
+//         console.log("Animal sleeps");
+//     }
+// }
+// class Dog extends Animal{
+    // sleep()
+    // {
+    //     console.log("Dog sleeps");
+    // }
+//     run()
+//     {
+//         console.log("Dog is running");
+//     }
+// }
+
+// const obj  = new Dog();
+// obj.sleep();
+// obj.run();
+
+class Animal{
+    message()
+    {
+        console.log("This is animal class");
+    }
+}
+class Dog extends Animal{
+    message()
+    {
+        console.log("This is Dog class");
+    }
+}
+
+const obj = new Dog();
+// const obj1 = new Animal();
+obj.message();
+// obj1.message();
